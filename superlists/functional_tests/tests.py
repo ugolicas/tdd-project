7ac9f032a8e53024f318b031ed318d9f5185bd93
igorlucas <igor.querino@enacom.com.br>
2020-10-18 19:40:14 -0300

    Integrando o Bootstrap

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,12 +1,13 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
+
 import time
 
 MAX_WAIT = 5
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Edith entra na home page
@@ -32,7 +33,7 @@ class NewVisitorTest(LiveServerTestCase):
             512,
             delta=10
         )
-        
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
