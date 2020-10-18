2292c4bd4b737dcba3ebfc88afc70fabf7d1669d
igorlucas <igor.querino@enacom.com.br>
2020-10-18 20:02:12 -0300

    Refatorando o Teste de Unidade

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -1,34 +1,12 @@
 from django.test import TestCase
 from lists.models import Item, List
 
-class ListAndItemModelsTest(TestCase):
 
-    def test_saving_and_retrieving_items(self):
-        my_list = List()
-        my_list.save()
+class HomePageTest(TestCase):
 
-        first_item = Item()
-        first_item.text = 'O primeiro item'
-        first_item.list = my_list
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'O segundo item'
-        second_item.list = my_list
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, my_list)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'O primeiro item')
-        self.assertEqual(first_saved_item.list, my_list)
-        self.assertEqual(second_saved_item.text, 'O segundo item')
-        self.assertEqual(second_saved_item.list, my_list)
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
 class ListViewTest(TestCase):
 
@@ -58,13 +36,6 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
         
-
-class HomePageTest(TestCase):
-
-    def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-
 
 class NewListTest(TestCase):
 
